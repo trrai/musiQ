560ccc4e69c549464fcc10d0605eeaf4be72e030
Bogdan Tudos <bogdantudos@gmail.com>
2018-10-20 21:19:23 -0700

    Add join button to home page

--- musiq/src/components/Home.js
+++ musiq/src/components/Home.js
@@ -9,8 +9,12 @@ class MusiQHome extends Component {
                 <h1>Musi<span id="Q">Q</span></h1>
                 <p id="subtitle">Collaborativly listen to music with your peers.</p>
                 <Card body className="getStartedCard, text-center">
-                    <CardTitle>Create or join a room and start building your music queue</CardTitle>
-                    <Link to="/create"><Button id="getStartedButton">Get Started</Button></Link>
+                    <CardTitle>Create a room and start building your music queue with the people around you.</CardTitle>
+                    <Link to="/create"><Button id="getStartedButton">Get started</Button></Link>
+                </Card>
+                <Card body className="getStartedCard, text-center">
+                    <CardTitle>Find a room created by your friends, peers, or an establishment.</CardTitle>
+                    <Link to="/join"><Button id="getStartedButton">Find a room</Button></Link>
                 </Card>
             </div>
         );
